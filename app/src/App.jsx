3a47be9db323cd9caf22ffb3b704c4ee3ed03e5e
alekspanteli <alekspanteli@rambler.ru>
2022-08-26 07:15:37 +0000

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">tst</div>;
-}
+const Avatar = () => {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+};
+
+const App = () => {
+  return <Avatar />;
+};
 
 export default App;
