d5d3cefeca4ec3c35a9a6b4649097657dde8f4a9
alekspanteli <alekspanteli@rambler.ru>
2022-08-26 07:13:42 +0000

    Update App.jsx and util.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,34 +1,7 @@
 import "./App.css";
-import { getImageUrl } from "./util.jsx";
-// import pic from "./assets/vite.svg";
-
-const Avatar = ({ person, size }) => {
-  return (
-    <img
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
-  s;
-};
-
-const Profile = () => {
-  return (
-    <>
-      <Avatar person={{ name: "Lin Lanying", imageId: "1bX5QH6" }} />
-    </>
-  );
-};
 
 function App() {
-  return (
-    <div className="App">
-      <Profile />
-    </div>
-  );
+  return <div className="App">tst</div>;
 }
 
 export default App;
--- app/src/util.jsx
+++
@@ -1,5 +0,0 @@
-import pic from "./assets/vite.svg";
-
-export function getImageUrl(person, size) {
-  return {pic} + person.imageId + size + ".jpg";
-}
